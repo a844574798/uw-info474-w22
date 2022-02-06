6d9a5c0ee0a7e1fb0e8df263c067e5430ced1980
David Wang <yw326@uw.edu>
2022-02-06 14:08:32 -0800

    Finished visualizations

--- src/App.jsx
+++ src/App.jsx
@@ -2,9 +2,11 @@ import { scaleLinear, scaleBand, timeParse, scaleTime, extent, line, symbol, csv
 import { AxisLeft, AxisBottom } from "@visx/axis";
 import { uniq } from "lodash";
 import senate2018 from "./2018_senate_seat_forecasets_538"
+import * as d3 from "d3";
 
 function App() {
   const tennessee = senate2018.filter(forecast => (forecast.state === "TN") && (forecast.model === "deluxe"));
+  const tennesseeD = senate2018.filter(forecast => (forecast.state === "TN") && (forecast.model === "deluxe") && (forecast.candidate === "Phil Bredesen"));
   const voteshareBredesen = [];
   const voteshareBlackburn = [];
   const voteshareOther = [];
@@ -46,7 +48,7 @@ function App() {
  };
 
   const voteMargin = absDifference(voteshareBredesen, voteshareBlackburn);
-  const voteshares = {"Bredesen(D)":voteshareBredesen, "Blackburn(R)":voteshareBlackburn, "Others":voteshareOther};
+  const voteshares = {"Bredesen(D)":voteshareBredesen, "Blackburn(R)":voteshareBlackburn};
   const winprobs = {"Bredesen(D)":winprobBredesen, "Blackburn(R)":winprobBlackburn};
   const candidates = ["Bredesen(D)", "Blackburn(R)"];
   const months = ["August", "September", "October", "November"];
@@ -75,6 +77,66 @@ function App() {
       return _scaleY(d);
     });
 
+
+
+  var minVoteShare = d3.min(voteshareBredesen);
+  var maxVoteShare = d3.max(voteshareBredesen);
+  var minWinProbD = d3.min(winprobBredesen);
+  var maxWinProbD = d3.max(winprobBredesen);
+
+
+  const voteShareChartWidth = 500;
+  const voteShareChartHeight = 200;
+  const voteShareMargin = 70;
+  const voteShareAxisTextPadding = 10;
+
+
+  const voteShareScale = scaleLinear()
+    .domain([minVoteShare, maxVoteShare])
+    .range([voteShareMargin, voteShareChartWidth - voteShareMargin - voteShareMargin]);
+  
+  const winProbScaleD = scaleLinear()
+    .domain([minWinProbD, maxWinProbD])
+    .range([voteShareMargin, voteShareChartWidth - voteShareMargin - voteShareMargin]);
+
+  var minVoteShareR = d3.min(voteshareBlackburn);
+  var maxVoteShareR = d3.max(voteshareBlackburn);
+  const voteShareScaleR = scaleLinear()
+    .domain([minVoteShareR, maxVoteShareR])
+    .range([voteShareMargin, voteShareChartWidth - voteShareMargin - voteShareMargin]);
+  
+  const _scaleVoteShare = scaleLinear()
+    .domain([40, 55])
+    .range([chartSize - margin, margin]);
+
+
+  const _lineMakerVote = line()
+    .x((d, i) => {
+      return _scaleLine(i);
+    })
+    .y((d) => {
+      return _scaleVoteShare(d);
+    });
+
+  const vBinGenerator = d3.bin().value((d) => d.voteshare);
+
+  const BredesenVoteBins = vBinGenerator(tennesseeD);
+  const voteShareBredBarHeightScale = scaleLinear()
+    .domain([0, d3.max(BredesenVoteBins, (d) => d.length)])
+    .range([
+      voteShareChartHeight - voteShareMargin - voteShareAxisTextPadding,
+      voteShareMargin,
+    ]);
+
+  const wBinGenerator = d3.bin().value((d) => d.win_probability);
+
+  const BredesenWinProbBins = wBinGenerator(tennesseeD);
+  const winProbBredBarHeightScale = scaleLinear()
+    .domain([0, d3.max(BredesenWinProbBins, (d) => d.length)])
+    .range([
+      voteShareChartHeight - voteShareMargin - voteShareAxisTextPadding,
+      voteShareMargin,
+    ]);
   return (
     <div style={{ margin: 20 }}>
       <h1>2018 Senate Forecast in the State of Tennessee</h1>
@@ -130,6 +192,301 @@ function App() {
             );
           })}
         </svg>
+        <svg
+          width={chartSize}
+          height={chartSize}
+          style={{ border: "1px solid black" }}
+        >
+          <AxisLeft strokeWidth={0} left={margin} scale={_scaleY} />
+          <AxisBottom
+            strokeWidth={0}
+            top={chartSize - margin}
+            left={20}
+            scale={voteShareScale}
+            numTicks={10}
+          />
+          <text x="50" y="50" fontSize={15}>
+            Correlation of voteshare and winning probability of Bredesen
+          </text>
+          <text x="-390" y="15" transform="rotate(-90)" fontSize={15}>
+            Senate Candidate Winning Probability Forecast
+          </text>
+          <text x="-390" y="30" transform="rotate(-90)" fontSize={15}>
+            from 08/01/2018-11/06/2018
+          </text>
+          <text x="100" y="475" fontSize={15}>
+            Senate Candidate Predicted Vote Share
+          </text>
+          <text x="100" y="490" fontSize={15}>
+            from 08/01/2018-11/06/2018
+          </text>
+          {voteshareBredesen.map((voteshare, i) => {
+            var winProb = winprobBredesen[i];
+            return (
+              <circle
+                key={i}
+                cx={voteShareScale(voteshare)}
+                cy={_scaleY(winProb)}
+                r={5}
+                style={{ stroke: "rgba(50,50,50)", fill: "none" }}
+              />
+            );
+
+          })}
+        </svg>
+      </div>
+      <div style={{ display: "flex" }}>
+        <svg
+            width={chartSize + legendPadding}
+            height={chartSize}
+            style={{ border: "1px solid black" }}
+          >
+            <AxisLeft strokeWidth={0} left={margin} scale={_scaleVoteShare} />
+            <AxisBottom
+              strokeWidth={0}
+              top={chartSize - margin}
+              left={margin}
+              scale={_scaleDate}
+              tickValues={testTime}
+            />
+            <text x="80" y="50" fontSize={20}>
+              Predicted Vote Share of Candidates Over Time
+            </text>
+            <text x="-390" y="15" transform="rotate(-90)" fontSize={15}>
+              Senate Candidate Vote Share Forecast
+            </text>
+            <text x="-390" y="30" transform="rotate(-90)" fontSize={15}>
+              from 08/01/2018-11/06/2018
+            </text>
+            {candidates.map((candidate, i) => {
+              return (
+                <path
+                  stroke={candidate === "Blackburn(R)" ? "red" : "blue"}
+                  strokeWidth={candidate === "Blackburn(R)" ? 3 : 1}
+                  fill="none"
+                  key={candidate}
+                  d={_lineMakerVote(voteshares[candidate])}
+                />
+              );
+            })}
+            {candidates.map((candidate, i) => {
+              return (
+                <text
+                  fill={"black"}
+                  style={{
+                    fontSize: 15,
+                    fontWeight: candidate === "Blackburn(R)" ? 700 : 300,
+                  }}
+                  key={`legend--${candidate}`}
+                  x={chartSize}
+                  y={_scaleVoteShare(voteshares[candidate][45])}
+                >
+                  {candidate}
+                </text>
+              );
+            })}
+          </svg>
+          
+        <svg
+          width={chartSize}
+          height={chartSize}
+          style={{ border: "1px solid black" }}
+        >
+          <AxisLeft strokeWidth={0} left={margin} scale={_scaleY} />
+          <AxisBottom
+            strokeWidth={0}
+            top={chartSize - margin}
+            left={20}
+            scale={voteShareScaleR}
+            numTicks={10}
+          />
+          <text x="50" y="50" fontSize={15}>
+            Correlation of voteshare and winning probability of Blackburn
+          </text>
+          <text x="-390" y="15" transform="rotate(-90)" fontSize={15}>
+            Senate Candidate Winning Probability Forecast
+          </text>
+          <text x="-390" y="30" transform="rotate(-90)" fontSize={15}>
+            from 08/01/2018-11/06/2018
+          </text>
+          <text x="100" y="475" fontSize={15}>
+            Senate Candidate Predicted Vote Share
+          </text>
+          <text x="100" y="490" fontSize={15}>
+            from 08/01/2018-11/06/2018
+          </text>
+          {voteshareBlackburn.map((voteshare, i) => {
+            var winProb = winprobBlackburn[i];
+            return (
+              <circle
+                key={i}
+                cx={voteShareScaleR(voteshare)}
+                cy={_scaleY(winProb)}
+                r={5}
+                style={{ stroke: "rgba(50,50,50)", fill: "none" }}
+              />
+            );
+
+          })}
+        </svg>
+      </div>
+      <p>Here we have a barcode plot of the predicted voteshare of Phil Bredesen:</p>
+      <div>
+        <svg
+          height={voteShareChartHeight}
+          width={voteShareChartWidth}
+          style={{ border: "1px solid black" }}
+        >
+          <text x="60" y="175" fontSize={15}>
+            Predicted Vote Share from 08/01/2018-11/06/2018
+          </text>
+          {voteshareBredesen.map((voteshare, i) => {
+            return (
+              <line
+                key={i}
+                x1={voteShareScale(voteshare)}
+                y1={voteShareMargin}
+                x2={voteShareScale(voteshare)}
+                y2={voteShareChartHeight / 2}
+                style={{ stroke: "rgba(70,130,180,.1)", fill: "none" }}
+              />
+            );
+          })}
+          <AxisBottom
+            strokeWidth={1}
+            top={voteShareChartHeight - voteShareMargin - voteShareAxisTextPadding}
+            scale={voteShareScale}
+            numTicks={7}
+          />
+        </svg>
+      </div>
+
+      <div>
+        <p>Here is a strip plot of the voteshares of Phil Bredesen:</p>
+        <svg
+            height={voteShareChartHeight}
+            width={voteShareChartWidth}
+            style={{ border: "1px solid black" }}
+          >
+            <text x="60" y="175" fontSize={15}>
+              Predicted Vote Share from 08/01/2018-11/06/2018
+            </text>
+            {voteshareBredesen.map((voteShares, i) => {
+              return (
+                <circle
+                  key={i}
+                  cx={voteShareScale(voteShares)}
+                  cy={voteShareChartHeight / 2}
+                  r={5}
+                  style={{ stroke: "rgba(50,50,50,.1)", fill: "none" }}
+                />
+              );
+            })}
+            <AxisBottom
+              strokeWidth={1}
+              top={voteShareChartHeight - voteShareMargin - voteShareAxisTextPadding}
+              scale={voteShareScale}
+              numTicks={10}
+            />
+          </svg>
+      </div>
+
+      <p>Here we show a histogram of predicted voteshare of Phil Bredesen.</p>
+      <div>
+        <svg width={voteShareChartWidth} height={voteShareChartHeight} style={{ border: "1px solid black" }}>
+          <text x="60" y="175" fontSize={15}>
+            Predicted Vote Share from 08/01/2018-11/06/2018
+          </text>
+          {BredesenVoteBins.map((bin, i) => {
+            return (
+              <rect
+                key={i}
+                fill="steelblue"
+                x={voteShareScale(bin.x0) + 1}
+                y={voteShareBredBarHeightScale(bin.length)}
+                width={Math.max(
+                  0,
+                  voteShareScale(bin.x1) - voteShareScale(bin.x0) - 1
+                )}
+                height={
+                  voteShareBredBarHeightScale(0) - voteShareBredBarHeightScale(bin.length)
+                }
+              />
+            );
+          })}
+          {BredesenVoteBins.map((bin, i) => {
+            return (
+              <text
+                key={i}
+                fill="black"
+                fontSize="10"
+                textAnchor="middle"
+                x={
+                  ((voteShareScale(bin.x0) + voteShareScale(bin.x1)) / 2) |
+                  0
+                }
+                y={voteShareBredBarHeightScale(bin.length) - 2}
+              >
+                {bin.length}
+              </text>
+            );
+          })}
+          <AxisBottom
+            strokeWidth={1}
+            top={voteShareChartHeight - voteShareMargin - voteShareAxisTextPadding}
+            scale={voteShareScale}
+            numTicks={7}
+          />
+        </svg>
+      </div>
+
+      <p>Here we show a histogram of predicted winning probability of Phil Bredesen.</p>
+      <div>
+        <svg width={voteShareChartWidth} height={voteShareChartHeight} style={{ border: "1px solid black" }}>
+          <text x="50" y="175" fontSize={15}>
+            Predicted winning probability from 08/01/2018-11/06/2018
+          </text>
+          {BredesenWinProbBins.map((bin, i) => {
+            return (
+              <rect
+                key={i}
+                fill="steelblue"
+                x={winProbScaleD(bin.x0) + 1}
+                y={winProbBredBarHeightScale(bin.length)}
+                width={Math.max(
+                  0,
+                  winProbScaleD(bin.x1) - winProbScaleD(bin.x0) - 1
+                )}
+                height={
+                  winProbBredBarHeightScale(0) - winProbBredBarHeightScale(bin.length)
+                }
+              />
+            );
+          })}
+          {BredesenWinProbBins.map((bin, i) => {
+            return (
+              <text
+                key={i}
+                fill="black"
+                fontSize="10"
+                textAnchor="middle"
+                x={
+                  ((winProbScaleD(bin.x0) + winProbScaleD(bin.x1)) / 2) |
+                  0
+                }
+                y={winProbBredBarHeightScale(bin.length) - 2}
+              >
+                {bin.length}
+              </text>
+            );
+          })}
+          <AxisBottom
+            strokeWidth={1}
+            top={voteShareChartHeight - voteShareMargin - voteShareAxisTextPadding}
+            scale={winProbScaleD}
+            numTicks={7}
+          />
+        </svg>
       </div>
       <p>This is a visualization of the Predicted Winning Probability of the <a href = "https://en.wikipedia.org/wiki/2018_United_States_Senate_election_in_Tennessee" target="_blank">2018 Senate 
         Election in the State of Tennessee</a>. The predicted data comes from 538, and 538 used their <a href = "https://fivethirtyeight.com/methodology/how-fivethirtyeights-house-and-senate-models-work/" target="_blank">
@@ -181,6 +538,7 @@ function App() {
       <p>
         Github Repository Link: <a href="https://github.com/a844574798/uw-info474-w22.git" target="_blank">https://github.com/a844574798/uw-info474-w22.git</a>
       </p>
+      
     </div>
   );
 }
